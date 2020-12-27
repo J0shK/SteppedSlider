cc92090d783fa4463285e344eca06e8c2c76071d
Josh Kowarsky <josh.kowarsky@gmail.com>
2020-12-26 19:39:14 -0800

    Highlight active tick

--- SteppedSlider/TickView.swift
+++ SteppedSlider/TickView.swift
@@ -11,22 +11,15 @@ struct TickView: View {
     @EnvironmentObject var viewModel: ViewModel
     
     let numTicks = 10
+    let offset: CGFloat = 10
+
     var body: some View {
         GeometryReader { geometry in
-            Path { path in
-                for i in 0...numTicks {
-                    let offset: CGFloat = 10
-                    let spacing = (geometry.size.width - (offset * 2)) / CGFloat(numTicks)
-                    let startX = (CGFloat(i) * spacing) + offset
-                    let stopX = startX + 1
-                    path.move(to: CGPoint(x: startX, y: 0))
-                    path.addLine(to: CGPoint(x: startX, y: geometry.size.height))
-                    path.addLine(to: CGPoint(x: stopX, y: geometry.size.height))
-                    path.addLine(to: CGPoint(x: stopX, y: 0))
-                    path.closeSubpath()
-                }
+            let spacing = (geometry.size.width - (offset * 2)) / CGFloat(numTicks)
+            ForEach(0..<numTicks + 1) { i in
+                let startX = (CGFloat(i) * spacing) + offset
+                Line(offset: startX, width: Int(viewModel.val * 10) == i ? 2 : 1, height: geometry.size.height, fill: Int(viewModel.val * 10) == i ? Color.white : Color.gray)
             }
-            .fill(Color.white)
         }
         .frame(minHeight: 10)
     }
@@ -37,5 +30,26 @@ struct TickView_Previews: PreviewProvider {
     static var previews: some View {
         TickView()
             .environmentObject(ViewModel())
+    }
+}
+
+
+struct Line: View {
+    let offset: CGFloat
+    let width: CGFloat
+    let height: CGFloat
+    let fill: Color
+
+    var body: some View {
+        Path { path in
+            let startX = offset - (width)
+            let stopX = offset + width
+            path.move(to: CGPoint(x: startX, y: 0))
+            path.addLine(to: CGPoint(x: startX, y: height))
+            path.addLine(to: CGPoint(x: stopX, y: height))
+            path.addLine(to: CGPoint(x: stopX, y: 0))
+            path.closeSubpath()
+        }
+        .fill(fill)
     }
 }
